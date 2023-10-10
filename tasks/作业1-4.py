9fc584f073deee0b7469b7341a4e1e4abeb0c244
yangjiueyue33 <yty431800@gmail.com>
2023-10-11 00:12:52 +0800

    第四次提交

--- tasks/作业1-4.py
+++ tasks/作业1-4.py
@@ -46,30 +46,50 @@ class Dual:
                 new_value = self.value ** float(exponent)
                 new_d = self.d * float(exponent) * self.value ** (float(exponent) - 1)
                 return Dual(new_value, new_d)
+             
+    def __rsub__(self, other: Union["Dual", Number]) -> "Dual":
+         match other:
+            case Dual(o_value, o_d):
+                return Dual(o_value - self.value, o_d - self.d)
+            case Number():
+                return Dual(float(other) - self.value, -self.d)
 
-    def sin(self):
-        return Dual(math.sin(self.value), math.cos(self.value)*self.d)
-
-    def cos(self):
-        return Dual(math.cos(self.value), -math.sin(self.value)*self.d)
-
-    def exp(self):
-        return Dual(math.exp(self.value), math.exp(self.value)*self.d)
-
-    def log(self):
-        return Dual(math.log(self.value), self.d/self.value)
+    def __float__(self):
+        return self.value
 
     __rmul__ = __mul__
     __radd__ = __add__
+
+def cos(x: Union[Dual, Number]) -> Dual:
+    if isinstance(x, Dual):
+        return Dual(math.cos(x.value), -math.sin(x.value)*x.d)
+    else:
+        return Dual(math.cos(x), 0)
+
+def sin(x: Union[Dual, Number]) -> Dual:
+    if isinstance(x, Dual):
+        return Dual(math.sin(x.value), math.cos(x.value)*x.d)
+    else:
+        return Dual(math.sin(x), 0)
+
+def exp(x: Union[Dual, Number]) -> Dual:
+    if isinstance(x, Dual):
+        return Dual(math.exp(x.value), math.exp(x.value)*x.d)
+    else:
+        return Dual(math.exp(x), 0)
+
+def log(x: Union[Dual, Number]) -> Dual:
+    if isinstance(x, Dual):
+        return Dual(math.log(x.value), x.d/x.value)
+    else:
+        return Dual(math.log(x), 0)
+
 
 def diff(func: Callable[[float], float]) -> Callable[[float], float]:
     return lambda x: func(Dual(x, 1.0)).d
 
 def f(x: Dual) -> Dual:
-    twelve = Dual(12.0, 0.0)  # 创建一个表示整数12的Dual对象
-    # twenty = Dual(20.0, 0.0)  # 创建一个表示整数20的Dual对象
-
-    return x + 5*x - (20 * (twelve - 20*x*x).log()).cos() - 20*x
+    return x + 5*x - cos(20 * log(12 - 20*x*x)) - 20*x
 
 df = diff(f)
-print(df(-0.5))  # 输出f在x=1.0处的导数
+print(df(-0.5))  # 输出f在x=-0.5处的导数  39.644950968544144
