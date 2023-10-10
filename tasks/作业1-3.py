01d92cc2842a11b10323c142b409d6fcdbec9fd7
yangjiueyue33 <yty431800@gmail.com>
2023-10-10 23:09:58 +0800

    第二次提交

--- tasks/作业1-3.py
+++ tasks/作业1-3.py
@@ -1,12 +1,9 @@
-from scipy.misc import derivative
-
+import math
 from typing import Callable
 
 # f′(x)≈ f(x+ϵ)−f(x−ϵ)​/2ϵ
 
-
 def numerical_derivative(func: Callable[[float], float], x: float, epsilon: float = 1e-6) -> float:
-
     f_x_plus_epsilon = func(x + epsilon)
     f_x_minus_epsilon = func(x - epsilon)
     
@@ -14,31 +11,16 @@ def numerical_derivative(func: Callable[[float], float], x: float, epsilon: floa
     
     return numerical_derivative
 
-# 获取用户输入的函数表达式
-expression = input("请输入函数表达式（使用 x 作为变量）：")
+def test_numerical_derivative():
+    # 定义你提供的函数
+    def func(x):
+        return x + 5*x - math.cos(20 * math.log(12- 20*x*x)) - 20*x
 
-# 获取用户输入的 x 值
-x_value = float(input("请输入 x 的值："))
-
-# 定义要求导的函数
-def f(x: float) -> float:
-    # 检查 x 是否为分母
-    if "/x" in expression:
-        if x_value == 0:
-            raise ValueError("x 不能为零，因为它是分母")
+    # 计算该函数在 x=0.5 处的数值导数
+    result = numerical_derivative(func, -0.5)
     
-    # 使用用户输入的表达式计算值
-    result = eval(expression)
-    return result
+    print(f"The derivative of the function at x=0.5 is {result}")
 
-try:
-    result = numerical_derivative(f,x_value)
-    print(f"函数在 x = {x_value} 处的导数为：{result}")
-except ValueError as e:
-    print(e)
+# 运行测试用例
+test_numerical_derivative()
 
-
-# def f(x: float) -> float:
-#     return 5 * x * x + 2 * x + 2
-
-# print(derivative(f, 2.))
